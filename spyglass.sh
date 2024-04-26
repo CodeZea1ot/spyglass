4956c33682049bc605309c42c3c122e2a96ac0c8
Tyler W. Phass (Code_Zealot) <business@codezealot.com>
2024-04-26 10:43:40 -0400

    Add stdin read support

--- spyglass.sh
+++ spyglass.sh
@@ -35,19 +35,52 @@ clear_previous_lines() {
 	local additional_lines=$((lines * spacing))
 	total_lines_to_clear=$((lines + additional_lines))
 	for ((i = 0; i < total_lines_to_clear; i++)); do
-		tput cuu1
-		tput el
+		tput rc # Restore the cursor position
+		tput ed # Clear everything below the cursor
 	done
 }
 
+run() {
+	((line_count++))
+
+	# Check if maximum number of lines is reached
+	# If it is, clear the lines that have been drawn to the terminal
+	if [[ $line_count -gt $max_lines ]]; then
+		apply_group_delay
+		if ! $keep_previous_group; then
+			clear_previous_lines $max_lines
+		fi
+		line_count=1
+	fi
+
+	# Save cursor position at the beginning of each group
+	if [[ $line_count -eq 1 ]]; then
+		tput sc
+	fi
+
+	# Output line
+	echo "$line"
+
+	# Output additional spaces if present
+	if [[ ! ${spacing} -eq 0 ]]; then
+		for ((i = 0; i < spacing; i++)); do
+			echo ""
+		done
+	fi
+
+	apply_line_delay
+}
+
 usage() {
-	echo "Usage: spyglass [-d] [-D] [-h] [-k] [-K] [-n] [-s] <file>"
-	echo "Options:"
+	echo -e "Usage:\n"
+	echo "	Read from stdin: <command-to-read-from> | spyglass [options]"
+	echo "	Read from file: spyglass [options] <file>"
+	echo -e "\nOptions:\n"
 	echo "	-d <line_delay>			Set the delay in seconds between each line output (default is ${line_delay})"
 	echo "	-D <group_delay>		Set the delay in seconds between each group of -n lines output (default is ${group_delay})"
 	echo "	-h <help>			Displays this usage menu"
-	echo "	-k <keep-final-group>			Keep the final group of output drawn to the terminal (default is to clear)"
-	echo "	-K <keep-all-groups>			Keep all output groups drawn to the terminal (default is to clear)"
+	echo "	-k <keep-final-group>		Keep the final group of output drawn to the terminal (default is to clear)"
+	echo "	-K <keep-all-groups>		Keep all output groups drawn to the terminal (default is to clear)"
 	echo "	-n <lines>			Specify the maximum number of lines to display as a group (default is ${max_lines})"
 	echo "	-s <spacing>			Set the amount of line breaks that should appear after each line (default is ${spacing})"
 	exit 1
@@ -56,11 +89,6 @@ usage() {
 ########## END FUNCTIONS ##########
 
 ########## MAIN ##########
-
-# Check if any arguments are provided
-if [[ $# -eq 0 ]]; then
-	usage
-fi
 
 # Parse command line options
 while getopts ":d:D:hkKn:s:" opt; do
@@ -103,39 +131,26 @@ shift $((OPTIND - 1))
 
 # Check if a file argument is provided
 if [[ $# -eq 0 ]]; then
-	echo "Error: Please provide a file." >&2
-	usage
-	exit 1
+	# Read from stdin if no arguments are provided
+	input="-"
+else
+	# Treat the first argument as a file
+	input="$1"
 fi
 
 line_count=0
-while IFS= read -r line || [[ -n "$line" ]]; do
 
-	((line_count++))
-
-	# Check if maximum number of lines is reached
-	# If it is, clear the lines that have been drawn to the terminal
-	if [[ $line_count -gt $max_lines ]]; then
-		apply_group_delay
-		if ! $keep_previous_group; then
-			clear_previous_lines $max_lines
-		fi
-		line_count=1
-	fi
-
-	# Output line
-	echo "$line"
-
-	# Output additional spaces if present
-	if [[ ! ${spacing} -eq 0 ]]; then
-		for ((i = 0; i < spacing; i++)); do
-			echo ""
-		done
-	fi
-
-	apply_line_delay
-
-done <"$1"
+if [[ "${input}" == "-" ]]; then
+	# Read from stdin
+	while IFS= read -r line || [[ -n "$line" ]]; do
+		run
+	done
+else
+	# Read from file
+	while IFS= read -r line || [[ -n "$line" ]]; do
+		run
+	done <"$input"
+fi
 
 # Ensure group delay is applied to final group
 apply_group_delay
